3d186820c88367d5f3df4f5ee43d8419c7fdde75
renzo-guerra <renzoguerralatt@gmail.com>
2023-10-23 18:48:02 -0300

    Inicio del servicio monopatin

---
+++ microservicio-monopatin/src/main/java/com/example/microserviciomonopatin/MicroservicioMonopatinApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviciomonopatin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioMonopatinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioMonopatinApplication.class, args);
+	}
+
+}
