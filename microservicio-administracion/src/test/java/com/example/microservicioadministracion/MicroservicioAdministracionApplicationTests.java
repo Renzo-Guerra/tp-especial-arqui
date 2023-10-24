1913390e74d5c45663ac2bc3a5965f4402571b09
Pedro Medina <pedrohugomedina@gmail.com>
2023-10-24 16:08:21 -0300

    cambios en archivos

---
+++ microservicio-administracion/src/test/java/com/example/microservicioadministracion/MicroservicioAdministracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicioadministracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioAdministracionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- microservicio-usuario/src/main/java/com/example/microserviciousuario/modelos/dto/UsuarioCreacion.java
+++ microservicio-usuario/src/main/java/com/example/microserviciousuario/modelos/dto/UsuarioCreacion.java
@@ -2,8 +2,10 @@ package com.example.microserviciousuario.modelos.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class UsuarioCreacion {
+public class UsuarioCreacion implements Serializable {
 
     private String nombre;
     private String apellido;
