b03a2710474602eb4fddfdf51549e774e903037c
Pedro Medina <pedrohugomedina@gmail.com>
2023-11-15 14:29:22 -0300

    Permisos microservicio usuario-cuenta

--- microservicio-usuario_cuenta/src/main/java/com/example/microserviciousuario/controladores/CuentaControlador.java
+++ microservicio-usuario_cuenta/src/main/java/com/example/microserviciousuario/controladores/CuentaControlador.java
@@ -1,10 +1,12 @@
 package com.example.microserviciousuario.controladores;
 
 import com.example.microserviciousuario.modelos.entidades.Cuenta;
+import com.example.microserviciousuario.security.AuthorityConstants;
 import com.example.microserviciousuario.servicios.CuentaServicio;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @Data
@@ -18,6 +20,7 @@ public class CuentaControlador {
      * Devuelve todas las cuentas
      */
     @GetMapping("")
+    @PreAuthorize( "hasAnyAuthority(\"" + AuthorityConstants.USER + "\" , \"" + AuthorityConstants.ADMIN + "\")" )
     public ResponseEntity<?> traerTodas(){
         try{
             return ResponseEntity.status(HttpStatus.OK).body(cuentaServicio.traerTodas());
@@ -30,6 +33,7 @@ public class CuentaControlador {
      * Devuelve una cuenta por ID
      */
     @GetMapping("/{id}")
+    @PreAuthorize( "hasAnyAuthority(\"" + AuthorityConstants.USER + "\" , \"" + AuthorityConstants.ADMIN + "\")" )
     public ResponseEntity<?> traerPorId(@PathVariable Long id){
         try{
             return ResponseEntity.status(HttpStatus.OK).body(cuentaServicio.traerPorId(id));
@@ -39,6 +43,7 @@ public class CuentaControlador {
     }
 
     @GetMapping("/{id_cuenta}/usuario/{id_usuario}")
+    @PreAuthorize( "hasAnyAuthority(\"" + AuthorityConstants.USER + "\" , \"" + AuthorityConstants.ADMIN + "\")" )
     public ResponseEntity<?> traerCuentaUsuarioPorId(@PathVariable Long id_cuenta, @PathVariable Long id_usuario){
         try{
             return ResponseEntity.status(HttpStatus.OK).body(cuentaServicio.traerCuentaUsuarioPorId(id_cuenta, id_usuario));
@@ -51,6 +56,7 @@ public class CuentaControlador {
      * Crea a una cuenta
      */
     @PostMapping("/{id_mercado_pago}")
+    @PreAuthorize( "hasAnyAuthority(\"" + AuthorityConstants.USER + "\" , \"" + AuthorityConstants.ADMIN + "\")" )
     public ResponseEntity<?> crearCuenta(@PathVariable Long id_mercado_pago){
         try{
             return ResponseEntity.status(HttpStatus.OK).body(cuentaServicio.crearCuenta(id_mercado_pago));
@@ -63,6 +69,7 @@ public class CuentaControlador {
      * Elimina una cuenta
      */
     @DeleteMapping("{id}")
+    @PreAuthorize( "hasAnyAuthority(\"" + AuthorityConstants.USER + "\" , \"" + AuthorityConstants.ADMIN + "\")" )
     public ResponseEntity<?> eliminarCuenta(@PathVariable Long id){
         try{
             return ResponseEntity.status(HttpStatus.OK).body(cuentaServicio.eliminarCuenta(id));
@@ -75,6 +82,7 @@ public class CuentaControlador {
      * Edita los datos de una cuenta
      */
     @PutMapping("/{id}")
+    @PreAuthorize( "hasAnyAuthority(\"" + AuthorityConstants.USER + "\" , \"" + AuthorityConstants.ADMIN + "\")" )
     public ResponseEntity<?> editarCuenta(@PathVariable Long id, @RequestBody Cuenta cuenta){
         try{
             return ResponseEntity.status(HttpStatus.OK).body(cuentaServicio.editarCuenta(id, cuenta));
@@ -84,6 +92,7 @@ public class CuentaControlador {
     }
 
     @PutMapping("{id_cuenta}/cargarSaldo/{monto}")
+    @PreAuthorize( "hasAnyAuthority(\"" + AuthorityConstants.USER + "\" , \"" + AuthorityConstants.ADMIN + "\")" )
     public ResponseEntity<?> cargarSaldo(@PathVariable Long id_cuenta, @PathVariable Double monto){
         try{
             return ResponseEntity.status(200).body(cuentaServicio.cargarSaldo(id_cuenta, monto));
@@ -93,6 +102,7 @@ public class CuentaControlador {
     }
 
     @PutMapping("{id_cuenta}/restarSaldo/{monto}")
+    @PreAuthorize( "hasAnyAuthority(\"" + AuthorityConstants.USER + "\" , \"" + AuthorityConstants.ADMIN + "\")" )
     public ResponseEntity<?> restarSaldo(@PathVariable Long id_cuenta, @PathVariable Double monto){
         try{
             return ResponseEntity.status(200).body(cuentaServicio.restarSaldo(id_cuenta, monto));
